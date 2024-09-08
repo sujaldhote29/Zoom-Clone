c4edc6ddf86bc69e773b17b17aabfa798cbd1440
Sujal <sujaldhote1@gmail.com>
2024-09-08 19:43:43 +0530

    all files added

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://sujaldhote29:W2FyknwhumDiLtvh@videocall.64oro.mongodb.net/")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
